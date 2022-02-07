484b38e245b112847b6489dbbd85bb73a99e02b4
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-02-07 12:41:37 -0300

    BrowserRoutes

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Routes } from "react-router";
+import { Routes, Route } from 'react-router-dom';
 
 import Home from '../components/home/Home'
 import User from "../components/users/User"
