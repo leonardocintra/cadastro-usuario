5c43ff2bf0f013865625aadb33986dbf07132bce
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-02-08 08:58:56 -0300

    add initial forms

--- src/components/pessoa/Pessoa.jsx
+++ src/components/pessoa/Pessoa.jsx
@@ -31,20 +31,68 @@ export default class Pessoa extends Component {
 
         axios[method](url, pessoa).then(resp => {
             const list = this.getUpdatedList(resp.data)
-            this.setState({ pessoa: initialState.pessoa, list})
+            this.setState({ pessoa: initialState.pessoa, list })
         })
     }
 
     getUpdatedList(pessoa) {
         const list = this.state.list.filter(p => p.id !== p.id)
+        // unshift é para colocar o novo item da lista como o primeiro da listagem
         list.unshift(pessoa)
         return list
+    }
+
+    updateField(event) {
+        const pessoa = { ...this.state.pessoa }
+        pessoa[event.target.nome] = event.target.value
+        this.setState({ pessoa })
+    }
+
+    renderForm() {
+        return (
+            <>
+                <div className="form">
+                    <div className="row">
+                        <div className="mb-3">
+                            <div className="form-group">
+                                <label className="form-label">Nome</label>
+                                <input type="text" className="form-control"
+                                    name="nome"
+                                    value={this.state.pessoa.nome}
+                                    onChange={e => this.updateField(e)}
+                                    placeholder="Digite o nome ..." />
+                            </div>
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">E-mail</label>
+                            <input type="text" className="form-control"
+                                name="email"
+                                value={this.state.pessoa.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o email" />
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary" onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+                        <button className="btn btn-danger ml-2" onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </>
+        )
     }
 
     render() {
         return (
             <Main {...headerProps}>
-                Cadastro de usuários 2
+                {this.renderForm()}
             </Main>
         )
     }
